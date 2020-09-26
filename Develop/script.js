a4dca31f12275f1a78ef9e712ce3559ff08fb2eb
Brian Lee <brianlee@Brians-MacBook-Pro.local>
2020-09-26 12:44:23 -0700

    timeblock template

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 var date = moment().format('dddd[,] MMMM Do');
-// var dow = moment().day
-// alert(dow);
 var time = $("#currentDay");
 time.text(date);
+
+var timeblock = $(".container");
+timeblock.append("9");
+for (var i = 10; i < 18 ; i++){
+    timeblock.append("<hr\>"+ i);
+}
