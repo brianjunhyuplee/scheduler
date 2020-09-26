64722d737363e215fc807b63f2a01268d4e9e77c
Brian Lee <brianlee@Brians-MacBook-Pro.local>
2020-09-26 15:30:13 -0700

    hour and formatting

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,20 @@
 $(document).ready(function(){
     var date = moment().format('dddd[,] MMMM Do');
-var time = $("#currentDay");
-time.text(date);
-
+var day = $("#currentDay");
+day.text(date);
+time = moment().hour();
+alert(time);
 var container = $(".container");
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+9+" AM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+10+" AM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+11+" AM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+12+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+1+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+2+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+3+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+4+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-// container.append('<div class = "row"><div class = "col-1" id = "time">'+5+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
+var contents9 = $("#contents9");
+var contents10 = $("#contents10");
+var contents11 = $("#contents11");
+var contents12 = $("#contents12");
+var contents12 = $("#contents13");
+var contents14 = $("#contents14");
+var contents15 = $("#contents15");
+var contents16 = $("#contents16");
+var contents17 = $("#contents17");
 
-for (var i = 9; i < 18; i++){
-   if(i<12){
-        container.append('<div class = "row"><div class = "col-1" id = "time">'+i+" AM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-    }
-    if(i==12){
-       container.append('<div class = "row"><div class = "col-1" id = "time">'+i+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-   }
-    if(i>12){
-       container.append('<div class = "row"><div class = "col-1" id = "time">'+(i-12)+" PM"+'</div><div class = "col-10" id = "contents"><textarea></textarea><div class = "col-1"><button id = "saveButton">save</button></div></div>');
-    }
-}
 
 
 });
